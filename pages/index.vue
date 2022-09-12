9ce837f7893e0c45b46f6062a74cbebe4cb2449c
Francis-Mwaniki <francismwaniki630@gmail.com>
2022-09-12 20:55:20 +0300

    church project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Icons</p>
+    <font-awesome-icon :icon="['fab', 'linkedin']" />
+    <font-awesome-icon :icon="['fab', 'github']" />
+    <font-awesome-icon :icon="['fab', 'facebook']" />
+    <font-awesome-icon :icon="['fas', 'coffee']" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
